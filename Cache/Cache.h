f58619ca427320286b9fb32b7fe60438561c313b
hamdanjaved <hamdan@kiddom.co>
2019-11-26 19:51:45 +0500

    add

---
+++ Cache/Cache.h
@@ -0,0 +1,19 @@
+//
+//  Cache.h
+//  Cache
+//
+//  Created by hamdan hashmi on 9/24/19.
+//  Copyright © 2019 Pinterest. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Cache.
+FOUNDATION_EXPORT double CacheVersionNumber;
+
+//! Project version string for Cache.
+FOUNDATION_EXPORT const unsigned char CacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cache/PublicHeader.h>
+
+
